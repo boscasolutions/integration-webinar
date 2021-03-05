3e500cc79ad37551de3f80be6a6ca4b221acf655
seanfarmar <s.farmar@gmail.com>
2021-03-05 19:36:30 +0000

    trying to control response

--- src/MapleWebApi/Controllers/OrderShipping.cs
+++ src/MapleWebApi/Controllers/OrderShipping.cs
@@ -26,20 +26,27 @@ namespace MapleWebApi.Controllers
             }
             else
             {
-                return BadRequest();
+                return new StatusCodeResult(Program.responseSet);
             }
         }
 
         [HttpPost]
         public async Task<ActionResult> Post([FromBody] OrderShipping model)
         {
-            var tracking = Guid.NewGuid().ToString();
-            model.TrackingNumber = tracking;
+            if (Program.responseSet == 200)
+            {
+                var tracking = Guid.NewGuid().ToString();
+                model.TrackingNumber = tracking;
 
-            var result = await _orderShippingService.Create(model).ConfigureAwait(false);
-            return CreatedAtAction(
-                nameof(GetByOrderById),
-                new { id = result.OrderId }, result);
+                var result = await _orderShippingService.Create(model).ConfigureAwait(false);
+                return CreatedAtAction(
+                    nameof(GetByOrderById),
+                    new { id = result.OrderId }, result);
+            }
+            else
+            {
+                return new StatusCodeResult(Program.responseSet);
+            }
         }
     }
 }
